876822220f23c319e7cc8ae824dc72214c214d82
Sherry Ummen <sherry.ummen@gmail.com>
2017-01-30 00:49:50 +0200

    Nothing in Common ( C# )

---
+++ C#/Nothing in Common/Nothing in Common/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//https://www.codechef.com/problems/NOTINCOM
+namespace Nothing_in_Common {
+    class Program {
+        static void Main(string[] args) {
+            int numberOfTestCases = Convert.ToInt32(Console.ReadLine());
+            while (numberOfTestCases-- != 0) {
+                int[] numberOfElementsEachHas = ReadSpaceSeperatedNumbersFromConsole();
+                int count = 0;
+                Dictionary<int, bool> temp = new Dictionary<int, bool>();
+                int[] aliceElements = ReadSpaceSeperatedNumbersFromConsole();
+                for (int i = 0; i < aliceElements.Length; i++) {
+                    temp[aliceElements[i]] = true;
+                }
+
+                int[] bertaElements = ReadSpaceSeperatedNumbersFromConsole();
+
+                for (int i = 0; i < bertaElements.Length; i++) {
+
+                    if (temp.ContainsKey(bertaElements[i]))
+                        count++;
+                }
+
+                Console.WriteLine(count);
+            }
+        }
+
+        private static int[] ReadSpaceSeperatedNumbersFromConsole() {
+            return Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x.TrimEnd().TrimStart())).ToArray();
+        }
+    }
+}
