d6e19fa6ad73b8741d69c4d322259f2011a2d2c3
Sherry Ummen <sherry.ummen@gmail.com>
2017-01-29 14:32:24 +0200

    Find next higher number with same digits ( C# )

---
+++ C#/NextHighestNumberWithSameDigits/NextHighestNumberTests/NextHighertNumberTests.cs
@@ -0,0 +1,42 @@
+﻿using NextHighestNumberWithSameDigits;
+using NUnit.Framework;
+
+namespace NextHighestNumberTests {
+    [TestFixture]
+    public class NextHigherNumberTests {
+
+        [Test]
+        public void TestSingleDigit() {
+            Assert.That(NextHigherNumber.GiveNextHighestNumber(1), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TestPalindromeDigit() {
+            Assert.That(NextHigherNumber.GiveNextHighestNumber(9999), Is.EqualTo(9999));
+        }
+
+        [Test]
+        public void TestDescendingorderedDigits() {
+            Assert.That(NextHigherNumber.GiveNextHighestNumber(54321), Is.EqualTo(54321));
+        }
+
+        [Test]
+        public void TestDNexthighestNumberDigit() {
+            Assert.That(NextHigherNumber.GiveNextHighestNumber(351), Is.EqualTo(513));
+            Assert.That(NextHigherNumber.GiveNextHighestNumber(218765), Is.EqualTo(251678));
+            Assert.That(NextHigherNumber.GiveNextHighestNumber(1234), Is.EqualTo(1243));
+            Assert.That(NextHigherNumber.GiveNextHighestNumber(534976), Is.EqualTo(536479));
+            Assert.That(NextHigherNumber.GiveNextHighestNumber(38276), Is.EqualTo(38627));
+            Assert.That(NextHigherNumber.GiveNextHighestNumber(45156), Is.EqualTo(45165));
+            Assert.That(NextHigherNumber.GiveNextHighestNumber(21897), Is.EqualTo(21978));
+            Assert.That(NextHigherNumber.GiveNextHighestNumber(19000), Is.EqualTo(90001));
+            Assert.That(NextHigherNumber.GiveNextHighestNumber(12345678), Is.EqualTo(12345687));
+        }
+
+        [Test]
+        public void TestBiggerNumbers() {
+            Assert.That(NextHigherNumber.GiveNextHighestNumber(987239879), Is.EqualTo(987239897));
+            Assert.That(NextHigherNumber.GiveNextHighestNumber(1234567980), Is.EqualTo(1234568079));
+        }
+    }
+}
---
+++ C#/NextHighestNumberWithSameDigits/NextHighestNumberWithSameDigits/NextHigherNumber.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace NextHighestNumberWithSameDigits {
+
+    public static class NextHigherNumber {
+
+        private static int ToNumber(this int[] source) => source.Aggregate((left, right) => (10 * left + right));
+
+        private static int[] ToDigits(this int source) => source.ToString().Select(x => Convert.ToInt32(x.ToString())).ToArray();
+
+        private static Action<int[], int, int> Swap = (int[] source, int indexFrom, int indexTo) => {
+            var temp = source[indexTo];
+            source[indexTo] = source[indexFrom];
+            source[indexFrom] = temp;
+        };
+
+        public static int GiveNextHighestNumber(int input) {
+
+            int[] splitValues = input.ToDigits();
+
+            int indexOfElementGreatThanPrevious = FindElementGreaterThanNextElementStartingFromRight(splitValues);
+
+            if (indexOfElementGreatThanPrevious == 0) return input;
+
+            int indexOfPivotElement = indexOfElementGreatThanPrevious - 1;
+            int indexOfElementNextToPivot = indexOfPivotElement + 1;
+            int indexOfSmallestButGreaterThanPivotAtTheRightOfPivot = indexOfPivotElement + 1;
+
+            indexOfSmallestButGreaterThanPivotAtTheRightOfPivot =
+                IndexOfSmallestButGreaterThanPivotAtTheRightOfPivot(splitValues,
+                                                                    indexOfPivotElement,
+                                                                    indexOfSmallestButGreaterThanPivotAtTheRightOfPivot);
+
+            Swap(splitValues, indexOfSmallestButGreaterThanPivotAtTheRightOfPivot, indexOfPivotElement);
+
+            var finalNumbers = splitValues.Take(indexOfElementNextToPivot).Concat(splitValues.Skip(indexOfElementNextToPivot).OrderBy(x => x)).ToArray();
+
+            return finalNumbers.ToNumber();
+        }
+
+        private static Func<int[], int> FindElementGreaterThanNextElementStartingFromRight = (int[] source) => {
+            int index = 0;
+            for (index = source.Length - 1; index > 0; index--) {
+                if (source[index] > source[index - 1])
+                    break;
+            }
+            return index;
+        };
+
+        private static int IndexOfSmallestButGreaterThanPivotAtTheRightOfPivot(int[] splitValues, int indexOfPivotElement, int indexOfSmallestButGreaterThanPivotAtTheRightOfPivot) {
+            int pivotElement = splitValues[indexOfPivotElement];
+            int i = 0;
+            for (i = indexOfPivotElement + 1; i < splitValues.Length; i++) {
+                if (splitValues[i] < splitValues[indexOfSmallestButGreaterThanPivotAtTheRightOfPivot] && splitValues[i] > pivotElement)
+                    indexOfSmallestButGreaterThanPivotAtTheRightOfPivot = i;
+            }
+
+            return indexOfSmallestButGreaterThanPivotAtTheRightOfPivot;
+        }
+    }
+}
